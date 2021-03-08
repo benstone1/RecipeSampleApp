f38d0148c3ebb6214bbf453f1e115eca6379af18
Ben Stone <benjamin.h.stone@gmail.com>
2021-03-08 15:47:33 -0500

    Initial Commit

---
+++ Swiftui-project-2-v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui-project-2-v1
+//
+//  Created by Ben Stone on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftui-project-2-v1/Swiftui_project_2_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_project_2_v1App.swift
+//  Swiftui-project-2-v1
+//
+//  Created by Ben Stone on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_project_2_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
